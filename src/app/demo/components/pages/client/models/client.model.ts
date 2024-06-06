479a0a4a672ff36841229d8d60f52c35000310d6
GustavoSS01 <gustavodssilva456@gmail.com>
2024-06-06 15:19:34 -0300

    feat(add-initial-client-crud)

---
+++ src/app/demo/components/pages/client/models/client.model.ts
@@ -0,0 +1,17 @@
+interface InventoryStatus {
+    label: string;
+    value: string;
+}
+export interface Client {
+    key?: string;
+    id?: string;
+    name?: string;
+    street?: string;
+    number?: number;
+    city?: string;
+    cep?: string;
+    state?: string;
+    phone?: string;
+    cpf?: string;
+    sex?: string;   
+}
